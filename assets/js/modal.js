3580a44b99aee403e54564d6c3b62f4f105346f5
Cristopherjoo <cristopherjoo@gmail.com>
2024-08-25 16:29:48 -0400

    commit

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
   $(".col-left, .col-right, .full-width").click(function () {
-    let proyecto = {
-      img: $(this).find("img").attr("src") || null,
-      nombre: $(this).find("h3").text(),
-      descripcion: $(this).find("p").text(),
-    };
 
-    let modalContent = `
-      <h5>${proyecto.nombre}</h5>
-      <p>${proyecto.descripcion}</p>`;
+    // Cambiar contenido del modal por opcion especifica
+    let title = $(this).find("h3").text();
+    let description = $(this).find("p").text();
 
-    if (proyecto.img) {
-      modalContent += `<img src="${proyecto.img}" alt="${proyecto.nombre}" class="img-fluid mt-3 ms-auto">`;
-    }
+    $("#modalProyectoLabel").text(title);
+    // $("#modalProyecto .modal-body").html("<p>" + description + "</p>");
+    $("#modalProyecto .modal-body").html(`<p> ${description} <p>`)
 
-    $("#modalProyecto .modal-body").html(modalContent);
-
-    $("#modalProyecto").modal("show");
+    // Mostrar el modal
+    $("#modalProyecto").modal('show');
   });
+  
 });
+
