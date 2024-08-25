3100e1b40f32be52055883d56a547fa4c6bde3b7
Cristopherjoo <cristopherjoo@gmail.com>
2024-08-25 17:12:54 -0400

    Revert "commit"
    This reverts commit 3580a44b99aee403e54564d6c3b62f4f105346f5.

--- portfolio/assets/js/modal.js
+++ portfolio/assets/js/modal.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
   $(".col-left, .col-right, .full-width").click(function () {
+    let proyecto = {
+      img: $(this).find("img").attr("src") || null,
+      nombre: $(this).find("h3").text(),
+      descripcion: $(this).find("p").text(),
+    };
 
-    // Cambiar contenido del modal por opcion especifica
-    let title = $(this).find("h3").text();
-    let description = $(this).find("p").text();
+    let modalContent = `
+      <h5>${proyecto.nombre}</h5>
+      <p>${proyecto.descripcion}</p>`;
 
-    $("#modalProyectoLabel").text(title);
-    // $("#modalProyecto .modal-body").html("<p>" + description + "</p>");
-    $("#modalProyecto .modal-body").html(`<p> ${description} <p>`)
+    if (proyecto.img) {
+      modalContent += `<img src="${proyecto.img}" alt="${proyecto.nombre}" class="img-fluid mt-3 ms-auto">`;
+    }
 
-    // Mostrar el modal
-    $("#modalProyecto").modal('show');
+    $("#modalProyecto .modal-body").html(modalContent);
+
+    $("#modalProyecto").modal("show");
   });
-  
 });
-
